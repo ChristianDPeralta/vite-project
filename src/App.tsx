ededae6562acfd665ccd323db83d357b38baa72c
ChristianDPeralta <christianperalta656@gmail.com>
2025-02-06 12:58:52 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <div className="name">Christian Dean M. Peralta</div>
+      <div className="box">
+        CPEITEL
+      </div>
+    </div>
+  );
+};
+
+export default App;
